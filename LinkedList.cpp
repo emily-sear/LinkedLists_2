691e53046dd9268bae519e1880bceda29259b930
Emily <emily.sear@cuw.edu>
2021-03-02 10:41:13 -0600

    changed stuff

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -54,7 +54,6 @@ int LinkedList::removeEnd()
 {
     if(this-> count > 0)
     {
-        
         Node* n = this->tail;
         int value = n->getPayload();
 
@@ -82,21 +81,19 @@ int LinkedList::removeEnd()
 }
 void LinkedList::addFront(int value)
 {
-    Node* n = new Node(value);
     if(head == NULL)
     {
-        //we have the empty list
-        this->head = n;
-        this->tail = n;
+        //same thing if head == NULL in addEnd()
+        this->addEnd(value);
+
     }
     else 
     {
-        //we have a list with at least one element in it, and we know that tail points to the
-        //end of the list
+        Node* n = new Node(value);
         n->setNextNode(this->head);
         this->head = n;
+        this->count++;
     }
-    this->count++;
 }
 
 int LinkedList::removeFront()
